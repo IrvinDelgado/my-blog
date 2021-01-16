14e313ff6f7bc7dd8818be8250375975c1f00cf5
IrvinDelgado <Idelgadoramirez@hawk.iit.edu>
2021-01-16 16:28:50 -0600

    adding icons

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,3 @@ document.getElementById('to-project-arrow').addEventListener('click', () => {
         behavior: 'smooth'
     });
 });
-
-document.getElementById('to-contact-arrow').addEventListener('click', () => {
-    document.querySelector('#contact').scrollIntoView({
-        behavior: 'smooth'
-    });
-});
